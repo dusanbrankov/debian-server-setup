643d038c224478c41bb880d1c69f1ea3bdce9fb1
Dušan Brankov <dusanbrankov@proton.me>
2024-07-13 09:10:49 +0200

    fix: redirect `echo`ed error message to stderr

--- db.backup.sh
+++ db.backup.sh
@@ -2,7 +2,7 @@
 
 # Ensure the script is run as root
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 2>&1
+   echo "This script must be run as root" >&2
    exit 1
 fi
 
@@ -23,7 +23,7 @@ databases=(
 for db in "${databases[@]}"; do
     backup_file="/tmp/$db.backup.$(date +%F-%H-%M).sql"
 
-    mysqldump --defaults-extra-file="$mysql_cnf" "$db" >"$backup_file" 
+    mysqldump --defaults-extra-file="$mysql_cnf" "$db" >"$backup_file"
     aws --profile eu2 --region default --endpoint-url "https://eu2.contabostorage.com/$bucket" s3 cp "$backup_file" "s3://$bucket"
 
     rm "$backup_file"
